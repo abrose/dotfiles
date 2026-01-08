164bcbc927d2b04024c784bd38c53a600df60366
Alfred Brose <hello@alfredbrose.dev>
2026-01-09 00:03:26 +0100

    feat: restructuring for stow

---
+++ neovim/.config/nvim-lazyvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
