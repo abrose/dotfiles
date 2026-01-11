7ddcd7c7738871c35931466224e059bf05bc6032
Alfred Brose <hello@alfredbrose.dev>
2026-01-11 18:27:15 +0100

    feat: change file explorer layout

---
+++ neovim/.config/nvim-lazyvim/lua/plugins/extend-snacks-explorer.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true, -- Show hidden/dotfiles by default
+            -- ignored = true,  -- Optionally also show gitignored files
+            layout = {
+              preset = "default",
+              preview = true,
+            },
+          },
+        },
+      },
+    },
+  },
+}
