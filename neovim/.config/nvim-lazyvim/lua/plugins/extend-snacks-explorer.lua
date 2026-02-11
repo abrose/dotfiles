27118f7c2fff3d2054e9294eec46d75df10a6efa
Alfred Brose <hello@alfredbrose.dev>
2026-02-11 11:16:39 +0100

    feat: styling floating terminal

--- neovim/.config/nvim-lazyvim/lua/plugins/extend-snacks-explorer.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/extend-snacks-explorer.lua
@@ -17,6 +17,9 @@ return {
               },
             },
           },
+          files = {
+            hidden = true,
+          },
         },
       },
     },
---
+++ neovim/.config/nvim-lazyvim/lua/plugins/extend-snacks-terminal.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    styles = {
+      terminal = {
+        position = "float",
+        border = "rounded",
+        width = 0.8,
+        height = 0.8,
+      },
+    },
+  },
+}
