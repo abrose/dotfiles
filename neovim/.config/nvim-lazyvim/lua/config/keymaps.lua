f09c885611456918af1d29a0fbc47456d35b4e13
Alfred Brose <hello@alfredbrose.dev>
2026-03-04 09:16:40 +0100

    feat: keymapping

--- neovim/.config/nvim-lazyvim/lua/config/keymaps.lua
+++ neovim/.config/nvim-lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>fy", function()
+  local path = vim.fn.expand("%:.") -- relative path
+  vim.fn.setreg("+", path)
+  Snacks.notify.info("Copied: " .. path)
+end, { desc = "Copy Relative Path" })
--- neovim/.config/nvim-lazyvim/lua/plugins/git-conflict.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/git-conflict.lua
@@ -2,25 +2,16 @@ return {
   "akinsho/git-conflict.nvim",
   lazy = false,
   opts = {
-    default_mappings = {
-      ours = "<leader>ho",
-      theirs = "<leader>ht",
-      none = "<leader>h0",
-      both = "<leader>hB",
-      next = "]x",
-      prev = "[x",
-    },
+    default_mappings = false, -- disable built-in mappings
   },
   keys = {
-    {
-      "<leader>gx",
-      "<cmd>GitConflictListQf<cr>",
-      desc = "List Conflicts",
-    },
-    {
-      "<leader>gr",
-      "<cmd>GitConflictRefresh<cr>",
-      desc = "Refresh Conflicts",
-    },
+    { "<leader>ho", "<cmd>GitConflictChooseOurs<cr>", desc = "Choose Ours" },
+    { "<leader>ht", "<cmd>GitConflictChooseTheirs<cr>", desc = "Choose Theirs" },
+    { "<leader>h0", "<cmd>GitConflictChooseNone<cr>", desc = "Choose None" },
+    { "<leader>hB", "<cmd>GitConflictChooseBoth<cr>", desc = "Choose Both" },
+    { "]x", "<cmd>GitConflictNextConflict<cr>", desc = "Next Conflict" },
+    { "[x", "<cmd>GitConflictPrevConflict<cr>", desc = "Prev Conflict" },
+    { "<leader>gx", "<cmd>GitConflictListQf<cr>", desc = "List Conflicts" },
+    { "<leader>gr", "<cmd>GitConflictRefresh<cr>", desc = "Refresh Conflicts" },
   },
 }
