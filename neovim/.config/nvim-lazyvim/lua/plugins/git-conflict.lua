2c89aee8d2c5de4e9a6d21f19bfaa4be87eebe9d
Alfred Brose <hello@alfredbrose.dev>
2026-03-24 14:37:15 +0100

    fix: fix git conflict keybinds

--- neovim/.config/nvim-lazyvim/lua/plugins/git-conflict.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/git-conflict.lua
@@ -2,15 +2,15 @@ return {
   "akinsho/git-conflict.nvim",
   lazy = false,
   opts = {
-    default_mappings = false, -- disable built-in mappings
+    default_mappings = true, -- let the plugin set up <Plug> mappings normally
   },
   keys = {
-    { "<leader>ho", "<cmd>GitConflictChooseOurs<cr>", desc = "Choose Ours" },
-    { "<leader>ht", "<cmd>GitConflictChooseTheirs<cr>", desc = "Choose Theirs" },
-    { "<leader>h0", "<cmd>GitConflictChooseNone<cr>", desc = "Choose None" },
-    { "<leader>hB", "<cmd>GitConflictChooseBoth<cr>", desc = "Choose Both" },
-    { "]x", "<cmd>GitConflictNextConflict<cr>", desc = "Next Conflict" },
-    { "[x", "<cmd>GitConflictPrevConflict<cr>", desc = "Prev Conflict" },
+    { "<leader>ho", "<Plug>(git-conflict-ours)", desc = "Choose Ours" },
+    { "<leader>ht", "<Plug>(git-conflict-theirs)", desc = "Choose Theirs" },
+    { "<leader>h0", "<Plug>(git-conflict-none)", desc = "Choose None" },
+    { "<leader>hB", "<Plug>(git-conflict-both)", desc = "Choose Both" },
+    { "]x", "<Plug>(git-conflict-next-conflict)", desc = "Next Conflict" },
+    { "[x", "<Plug>(git-conflict-prev-conflict)", desc = "Prev Conflict" },
     { "<leader>gx", "<cmd>GitConflictListQf<cr>", desc = "List Conflicts" },
     { "<leader>gr", "<cmd>GitConflictRefresh<cr>", desc = "Refresh Conflicts" },
   },
