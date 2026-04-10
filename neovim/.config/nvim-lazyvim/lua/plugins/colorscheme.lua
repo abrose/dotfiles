613ab94284387bc204ec52f51d02f7106b6b0f68
Alfred Brose <hello@alfredbrose.dev>
2026-04-10 14:55:26 +0200

    feat: modify git highligh colors

--- neovim/.config/nvim-lazyvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/colorscheme.lua
@@ -2,10 +2,17 @@ return {
   {
     "folke/tokyonight.nvim",
     opts = {
-      transparent = true,
+      transparent = false,
       styles = {
-        sidebars = "transparent",
+        sidebars = "normal",
       },
+      on_highlights = function(hl, c)
+        -- Customize diff highlights (used by mini.diff overlay via links)
+        hl.DiffAdd = { bg = "#1a3a14" }
+        hl.DiffDelete = { bg = "#440c0c" }
+        hl.DiffChange = { bg = "#3a3606" }
+        hl.DiffText = { bg = "#4a4608" }
+      end,
     },
   },
   {
