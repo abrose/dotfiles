c195b650b2058eb42b16d37fa361dc6c2838a5df
Alfred Brose <hello@alfredbrose.dev>
2026-04-12 18:27:20 +0200

    feat: update and simplify oxc config

--- neovim/.config/nvim-lazyvim/lua/plugins/conform.lua
+++ neovim/.config/nvim-lazyvim/lua/plugins/conform.lua
@@ -2,10 +2,6 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      typescript = { "oxfmt" },
-      typescriptreact = { "oxfmt" },
-      javascript = { "oxfmt" },
-      javascriptreact = { "oxfmt" },
       json = { "prettier" },
       markdown = { "prettier" },
       yaml = { "prettier" },
