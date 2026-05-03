31ca58961856f55af8c3394ec3aa6080e6305c58
Alfred Brose <hello@alfredbrose.dev>
2026-05-03 11:53:56 +0200

    feat: hunk management with mini.diff -> remove gitsigns

---
+++ neovim/.config/nvim-lazyvim/lua/plugins/extend-mini-diff.lua
@@ -0,0 +1,35 @@
+return {
+  "echasnovski/mini.diff",
+  keys = {
+    {
+      "<leader>ghs",
+      function()
+        require("mini.diff").do_hunks(0, "apply")
+      end,
+      desc = "Stage Hunk",
+    },
+    {
+      "<leader>ghr",
+      function()
+        require("mini.diff").do_hunks(0, "reset")
+      end,
+      desc = "Reset Hunk",
+    },
+    {
+      "<leader>ghs",
+      function()
+        require("mini.diff").do_hunks(0, "apply")
+      end,
+      mode = "x",
+      desc = "Stage Selection",
+    },
+    {
+      "<leader>ghr",
+      function()
+        require("mini.diff").do_hunks(0, "reset")
+      end,
+      mode = "x",
+      desc = "Reset Selection",
+    },
+  },
+}
